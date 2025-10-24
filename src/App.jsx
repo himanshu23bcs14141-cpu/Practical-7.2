37ab98845d0dde109ae4e21e179d42775a065a5f
himanshu23bcs14141-cpu <himanshu23bcs14141@gmail.com>
2025-10-24 23:16:25 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+
+function App() {
+  return (
+    <div>
+      <ProductList />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
